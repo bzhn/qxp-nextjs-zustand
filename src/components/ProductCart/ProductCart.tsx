42ad06a82a02f2266cffe82720a87d55343ebdc1
bzhn <bzhn@duck.com>
2023-10-29 16:31:46 +0200

    Added increment, decrement and remove actions

--- src/components/ProductCart/ProductCart.tsx
+++ src/components/ProductCart/ProductCart.tsx
@@ -10,15 +10,67 @@ const ProductCart = () => {
   );
 
   if (!cartStore) return <div></div>;
-  const { cart } = cartStore;
+  const { cart, incrementItem, decrementItem, removeItem } = cartStore;
+
+  const handleIncrementItem =
+    (id: number) => (e: React.MouseEvent<any, MouseEvent>) => {
+      incrementItem(id);
+    };
+
+  const handleDecrementItem =
+    (id: number) => (e: React.MouseEvent<any, MouseEvent>) => {
+      decrementItem(id);
+    };
+
+  const handleRemoveItem =
+    (id: number) => (e: React.MouseEvent<any, MouseEvent>) => {
+      removeItem(id);
+    };
 
   return (
     <div>
-      {cart.map((product, idx) => (
-        <div key={idx}>
-          {product.count} pcs. [ID: {product.id}] - {product.title}
-        </div>
-      ))}
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>Product ID</th>
+            <th>Title</th>
+            <th>Quantity</th>
+            <th>Actions</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((product, idx) => (
+            <tr key={idx}>
+              <td>{product.id}</td>
+              <td>{product.title}</td>
+              <td>{product.count} pcs.</td>
+              <td>
+                <button
+                  onClick={handleIncrementItem(product.id)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded"
+                >
+                  +
+                </button>
+                <button
+                  onClick={handleDecrementItem(product.id)}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-1 rounded ml-2"
+                >
+                  -
+                </button>
+              </td>
+              <td>
+                <button
+                  onClick={handleRemoveItem(product.id)}
+                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-1 rounded ml-2"
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
